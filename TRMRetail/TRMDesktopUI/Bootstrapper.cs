26e400b9b53ac56300d1644e1a8219a06d6fcbe8
Mücahit Özkanca <mmucahit17@hotmail.com>
2023-12-10 10:36:32 +0300

    View models and caliburn added v-6

---
+++ TRMRetail/TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+	public class Bootstrapper : BootstrapperBase
+	{
+		protected override void OnStartup(object sender, StartupEventArgs e)
+		{
+			Task.Run(() => DisplayRootViewForAsync<ShellViewModel>()).Wait();
+		}
+	}
+}
