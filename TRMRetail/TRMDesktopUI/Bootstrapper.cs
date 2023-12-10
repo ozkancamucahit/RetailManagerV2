c70e35b5043229bb2086923a299f6eeec5a1b718
Mücahit Özkanca <mmucahit17@hotmail.com>
2023-12-10 11:24:36 +0300

    bootstrapper configured v-7

--- TRMRetail/TRMDesktopUI/Bootstrapper.cs
+++ TRMRetail/TRMDesktopUI/Bootstrapper.cs
@@ -11,9 +11,57 @@ namespace TRMDesktopUI
 {
 	public class Bootstrapper : BootstrapperBase
 	{
+
+		#region FIELDS
+		private readonly SimpleContainer _container = new SimpleContainer();
+
+		public Bootstrapper(SimpleContainer container)
+		{
+			_container = container;
+		}
+
+		#endregion
+
 		protected override void OnStartup(object sender, StartupEventArgs e)
 		{
 			Task.Run(() => DisplayRootViewForAsync<ShellViewModel>()).Wait();
+		}
+
+		protected override void Configure()
+		{
+			_container
+				.Instance(_container);
+				//.PerRequest<IProductEndPoint, ProductEndPoint>();
+
+			_container
+					.Singleton<IWindowManager, WindowManager>()
+					.Singleton<IEventAggregator, EventAggregator>();
+					//.Singleton<ILoggedInUserModel, LoggedInUserModel>()
+					//.Singleton<IConfigHelper, ConfigHelper>()
+					//.Singleton<IAPIHelper, APIHelper>();
+
+			GetType().Assembly.GetTypes()
+				.Where(type => type.IsClass)
+				.Where(type => type.Name.EndsWith("ViewModel"))
+				.ToList()
+				.ForEach(viewModelType => _container.RegisterPerRequest(
+					viewModelType, viewModelType.ToString(), viewModelType));
+
+		}
+
+		protected override object GetInstance(Type service, string key)
+		{
+			return _container.GetInstance(service, key);
+		}
+
+		protected override IEnumerable<object> GetAllInstances(Type service)
+		{
+			return _container.GetAllInstances(service);
+		}
+
+		protected override void BuildUp(object instance)
+		{
+			_container.BuildUp(instance);
 		}
 	}
 }
