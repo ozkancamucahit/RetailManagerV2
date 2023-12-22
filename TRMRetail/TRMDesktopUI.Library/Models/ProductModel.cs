450c88f84f7dc3a2e194dfd8a94f251d66e64c40
Mücahit Özkanca <mmucahit17@hotmail.com>
2023-12-22 20:00:10 +0300

    SalesUI added v-15

---
+++ TRMRetail/TRMDesktopUI.Library/Models/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.Library.Models
+{
+	public sealed class ProductModel
+	{
+		public int Id { get; set; }
+		public string ProductName { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public decimal RetailPrice { get; set; }
+		public int QuantityInStock { get; set; }
+		public bool IsTaxable { get; set; }
+
+	}
+}
--- TRMRetail/TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMRetail/TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -1,12 +1,218 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Caliburn.Micro;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using TRMDesktopUI.Library.Models;
 
 namespace TRMDesktopUI.ViewModels
 {
-	public class SalesViewModel
+	public sealed class SalesViewModel : Screen
 	{
+		#region Fields
+		private BindingList<ProductModel> _products;
+		private IProductEndPoint _productEndPoint;
+		private IConfigHelper _configHelper;
+
+		public BindingList<ProductModel> Products
+		{
+			get { return _products; }
+			set
+			{
+				_products = value;
+				NotifyOfPropertyChange(() => Products);
+			}
+		}
+
+		private ProductModel _selectedRoduct;
+
+		public ProductModel SelectedProduct
+		{
+			get { return _selectedRoduct; }
+			set
+			{
+				_selectedRoduct = value;
+				NotifyOfPropertyChange(() => SelectedProduct);
+				NotifyOfPropertyChange(() => CanAddToCart);
+
+			}
+		}
+
+
+		private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+		public BindingList<CartItemModel> Cart
+		{
+			get { return _cart; }
+			set
+			{
+				_cart = value;
+				NotifyOfPropertyChange(() => Cart);
+			}
+		}
+
+
+		private int _itemQuantity = 1;
+
+		public int ItemQuantity
+		{
+			get { return _itemQuantity; }
+			set
+			{
+				_itemQuantity = value;
+				NotifyOfPropertyChange(() => ItemQuantity);
+				NotifyOfPropertyChange(() => CanAddToCart);
+			}
+		}
+
+		public bool CanAddToCart
+		{
+			get
+			{
+				bool output = false;
+
+				if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+				{
+					output = true;
+				}
+
+				return output;
+			}
+		}
+
+		public bool CanRemoveFromCart
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+		public bool CanCheckOut
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+
+		public string SubTotal
+		{
+			get
+			{
+				decimal subTotal = CalculateSubTotal();
+
+
+				return subTotal.ToString("C");
+			}
+		}
+
+		private decimal CalculateSubTotal()
+		{
+			return Cart.Sum(M => M.Product.RetailPrice * M.QuantityInCart);
+		}
+
+		public string Total
+		{
+			get
+			{
+				decimal total = CalculateTax() + CalculateSubTotal();
+				return total.ToString("C");
+			}
+		}
+
+		public string Tax
+		{
+			get
+			{
+				decimal taxAmount = CalculateTax();
+
+				return taxAmount.ToString("C");
+			}
+		}
+
+		private decimal CalculateTax()
+		{
+			decimal taxRate = _configHelper.GetTaxRate() / 100;
+			return Cart
+					.Where(m => m.Product.IsTaxable)
+					.Sum(M => M.Product.RetailPrice * M.QuantityInCart * taxRate);
+		}
+
+
+		#endregion
+
+		#region CTOR
+		public SalesViewModel(IProductEndPoint productEndPoint, IConfigHelper configHelper)
+		{
+			_productEndPoint = productEndPoint;
+			_configHelper = configHelper;
+		}
+		#endregion
+
+
+		#region Funcs
+		public void AddToCart()
+		{
+
+			CartItemModel existingModel = Cart.FirstOrDefault(m => m.Product == SelectedProduct);
+
+			if (existingModel != null)
+			{
+				existingModel.QuantityInCart += ItemQuantity;
+				Cart.Remove(existingModel);
+				Cart.Add(existingModel);
+			}
+			else
+			{
+				var item = new CartItemModel()
+				{
+					Product = SelectedProduct,
+					QuantityInCart = ItemQuantity
+				};
+
+				Cart?.Add(item);
+			}
+
+			SelectedProduct.QuantityInStock -= ItemQuantity;
+			ItemQuantity = 1;
+			NotifyOfPropertyChange(() => SubTotal);
+			NotifyOfPropertyChange(() => Cart);
+			NotifyOfPropertyChange(() => Total);
+			NotifyOfPropertyChange(() => Tax);
+		}
+
+		public void RemoveFromCart()
+		{
+			NotifyOfPropertyChange(() => SubTotal);
+			NotifyOfPropertyChange(() => Tax);
+			NotifyOfPropertyChange(() => Total);
+
+		}
+
+		public void CheckOut()
+		{
+
+		}
+
+		private async Task LoadProducts()
+		{
+			var productsList = await _productEndPoint.GetProducts();
+			Products = new BindingList<ProductModel>(productsList.ToList());
+		}
+
+		protected override async void OnViewLoaded(object view)
+		{
+			base.OnViewLoaded(view);
+			await LoadProducts();
+		}
+
+		#endregion
+
+
 	}
 }
