283adffd952f272e7904f5c8e7fc66d245d9bd7a
Mücahit Özkanca <mmucahit17@hotmail.com>
2023-12-26 10:56:15 +0300

    wired up login and sales form

--- TRMRetail/TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMRetail/TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -12,37 +12,24 @@ namespace TRMDesktopUI.ViewModels
 
 		#endregion
 
-		//public ShellViewModel(LoginViewModel loginViewModel)
-		//{
-		//	this.loginViewModel = loginViewModel;
-		//	ActivateItem(this.loginViewModel);
-		//}
-
 		public ShellViewModel(
 							  IEventAggregator events,
-							  //SalesViewModel salesViewModel
+							  SalesViewModel salesViewModel,
 							  LoginViewModel loginViewModel
 			)
 		{
 			_events = events;
-			//_salesViewModel = salesViewModel;
+			_salesViewModel = salesViewModel;
 			_loginViewModel = loginViewModel;
+
 			_events.Subscribe(this);
 			ActivateItem(IoC.Get<LoginViewModel>());
 		}
 
 		public void Handle(LogOnEvent message)
 		{
-			ActivateItem(_loginViewModel);
+			ActivateItem(_salesViewModel);
 		}
-
-
-
-		//public async Task HandleAsync(LogOnEvent message, CancellationToken cancellationToken)
-		//{
-		//	await ActivateItemAsync(_salesViewModel);
-		//	//return Task.CompletedTask;
-		//}
 
 	}
 }
