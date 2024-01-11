0cf62fe81db3af27e01df72339cacf8a5db21e01
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-11 10:42:40 +0300

    json properties for authenticated user

--- TRMRetail/TRMDesktopUI.Library/Models/AuthenticatedUser.cs
+++ TRMRetail/TRMDesktopUI.Library/Models/AuthenticatedUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,10 @@ namespace TRMDesktopUI.Library.Models
 {
 	public class AuthenticatedUser
 	{
+		[JsonProperty("acces_Token")]
 		public string access_token { get; set; }
 
+		[JsonProperty("userName")]
 		public string UserName { get; set; }
 	}
 }
