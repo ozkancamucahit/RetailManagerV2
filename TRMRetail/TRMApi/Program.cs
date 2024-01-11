bcc2a76ef00be73404cc0cb31c015e0a4fcdb746
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-11 10:45:09 +0300

    JWT configuration added

--- TRMRetail/TRMApi/Program.cs
+++ TRMRetail/TRMApi/Program.cs
@@ -1,20 +1,57 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TRMApi.Data;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services
+	.AddDbContext<ApplicationDbContext>(options =>
+		options.UseSqlServer(connectionString))
+	.AddDatabaseDeveloperPageExceptionFilter()
+	.AddAuthentication(opt =>
+	{
+		opt.DefaultAuthenticateScheme = "JwtBearer";
+		opt.DefaultChallengeScheme = "JwtBearer";
+
+	}).AddJwtBearer("JwtBearer", jwtOptions =>
+	{
+		jwtOptions.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication")),
+			ValidateIssuer = false,
+			ValidateAudience = false,
+			ValidateLifetime = true,
+			ClockSkew = TimeSpan.FromMinutes(5) // tolerance for time checks
+		};
+	});
 
 builder.Services
 	.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
 	.AddRoles<IdentityRole>()
 	.AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSwaggerGen(setup =>
+{
+	setup.SwaggerDoc(
+		"v1",
+		new OpenApiInfo
+		{
+			Title = "MECCU RETAIL API",
+			Version = "v1"
+		}
+		);
+});
+
 
 var app = builder.Build();
 
@@ -30,13 +67,20 @@ else
 	app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI( x =>
+{
+	x.SwaggerEndpoint("/swagger/v1/swagger.json",
+		"Timco API v1");
+});
 
 app.MapControllerRoute(
 	name: "default",
